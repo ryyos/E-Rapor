2a196f4b04d74bc6da36044bdcb36f0119b2a172
ryosyntaxx <riodwi12174@gmail.com>
2023-11-12 02:45:25 +0700

    first commit

---
+++ src/main/java/com/erapor/erapor/ERaporApplication.java
@@ -0,0 +1,13 @@
+package com.erapor.erapor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERaporApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERaporApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erapor/erapor/ERaporApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erapor.erapor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERaporApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
