a61bbbe0af6213c6ed0ca36c0375c9b981136123
ryosyntaxx <riodwi12174@gmail.com>
2023-11-12 19:52:28 +0700

    update fetch

--- src/main/java/com/erapor/erapor/repository/StudentsRepository.java
+++ src/main/java/com/erapor/erapor/repository/StudentsRepository.java
@@ -8,19 +8,7 @@ import java.util.List;
 
 public interface StudentsRepository extends JpaRepository<StudentsDAO, String> {
 
-    @Query("SELECT s FROM StudentsDAO s JOIN s.valuesDAO val ORDER BY val.TOTAL DESC")
+    @Query("SELECT s FROM StudentsDAO s JOIN FETCH s.valuesDAO val ORDER BY val.TOTAL DESC")
     List<StudentsDAO> findRanking();
 
 }
-
-/*SELECT
-        s.name,
-        (v.MTK + v.IPS + v.IPA) AS total_value,
-        v.MTK,
-        v.IPS,
-        v.IPA
-FROM students AS s
-JOIN `values` AS v
-        ON s.id = v.student_id
-ORDER BY total_value DESC LIMIT 10 ;
-*/
